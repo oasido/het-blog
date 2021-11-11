ab8e89e9e11dc1d244bd0cff7f3f2304972de99d
Ofek Asido <asido.ofek@gmail.com>
2021-11-12 00:46:30 +0200

    ➕ Add blog page component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Create from './components/Create';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BlogItself from './components/BlogItself';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/create" element={<Create />} />
+            <Route exact path="/blogs/:id" element={<BlogItself />} />
           </Routes>
         </div>
       </div>
---
+++ src/components/BlogItself.jsx
@@ -0,0 +1,29 @@
+import { useParams } from 'react-router';
+import useFetch from './useFetch';
+import ReactLoading from 'react-loading';
+
+const BlogItself = () => {
+  const { id } = useParams();
+  const { data: blog, isLoading } = useFetch('http://localhost:8000/blogs/' + id);
+
+  return (
+    <div>
+      {isLoading && (
+        <div>
+          <ReactLoading className="loading" color="#000" type="spin" />
+          <div className="loading">Loading, please wait...</div>
+        </div>
+      )}
+      <div className="blog-details">
+        {blog && (
+          <article>
+            <h2>{blog.title}</h2>
+            <p>{blog.body}</p>
+          </article>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BlogItself;
