ef8e706dd5683269328b588fbd7a862a531ee282
Ofek Asido <asido.ofek@gmail.com>
2021-11-14 22:58:42 +0200

    Add passport-local-mongoose

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+var passport = require('passport');
 const Blog = require('./models/Blog.js');
-
+const User = require('./models/User.js');
 const app = express();
 const port = 3080;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost/reactblog').catch((err) => console.log(err.reason));
 
@@ -12,6 +16,22 @@ app.get('/ping', async (req, res) => {
     res.send('Pong!');
     const blog = await Blog.create({ title: 'yo', author: 'this is a body', body: 'i did it' });
     console.log('aye');
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Passport-local-mongoose
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.post('/register', async (req, res) => {
+  try {
+    const { username, email, password, confirmPassword } = req.body;
+    if (password !== confirmPassword) {
+      res.send('NO_MATCH'); // TODO: Figure out how to send messages to the Front End, without refreshing the page
+    }
   } catch (error) {
     console.log(error);
   }
