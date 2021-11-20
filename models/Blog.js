4e630db61dc80f90d8e8f031049cbe0c378afeea
Ofek Asido <asido.ofek@gmail.com>
2021-11-20 21:03:15 +0200

    Add date by default to Blog schema

--- models/Blog.js
+++ models/Blog.js
@@ -4,7 +4,7 @@ const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   body: { type: String, required: true },
-  // date: { type: Date, default: Date.now },
+  date: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
