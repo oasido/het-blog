0c2fe6c9f47d19c0949608cd55ebd6b63b9dea5a
Ofek Asido <asido.ofek@gmail.com>
2021-11-26 13:41:11 +0200

    Schema cleanup

--- models/Blog.js
+++ models/Blog.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const blogSchema = new mongoose.Schema({
+const blogSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   body: { type: String, required: true },
