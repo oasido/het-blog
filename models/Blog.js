db3b92234077eb7023ac0f9ebead46470705002c
Ofek Asido <asido.ofek@gmail.com>
2021-11-27 03:15:03 +0200

    Add email to blog schema & change date to string

--- models/Blog.js
+++ models/Blog.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
   body: { type: String, required: true },
-  date: { type: Date, default: Date.now },
+  author: { type: String, required: true },
+  email: { type: String, required: true },
+  date: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
