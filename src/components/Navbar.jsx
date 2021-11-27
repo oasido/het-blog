60416acdb58dc688c9c2636f9a2d2c700a0ef938
Ofek Asido <asido.ofek@gmail.com>
2021-11-27 03:30:07 +0200

    Rearrange Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,19 +16,24 @@ function Navbar({ user }) {
 
         <div className="links">
           <div className="dropdown">
-            {isAuthenticated && (
-              <Link to="/create" className="inverted">
-                New Blog
-              </Link>
-            )}
             <span id="menu-text">Menu</span>
             <div className="dropdown-content">
               {!isAuthenticated && <Link to="/login">Login</Link>}
               {!isAuthenticated && <Link to="/register">Register</Link>}
               {isAuthenticated && <p>Hello, {username}</p>}
-              {isAuthenticated && <a href="/logout">Log Out</a>}
+              {isAuthenticated && <a href="/my-posts">My Posts</a>}
+              {isAuthenticated && (
+                <a className="color-danger" href="/logout">
+                  Log Out
+                </a>
+              )}
             </div>
           </div>
+          {isAuthenticated && (
+            <Link to="/create" className="inverted">
+              Create Post
+            </Link>
+          )}
         </div>
       </nav>
     </div>
