d6f7a93be3057da8067ff40b249e430ed5f7fe6f
Ofek Asido <asido.ofek@gmail.com>
2021-12-10 21:48:31 +0200

    Update NotFound component to be modular with users

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
 
-const NotFound = () => {
+const NotFound = ({ user }) => {
   return (
     <div className="not-found">
-      <h2>Page Not Found</h2>
-      <p>Sorry, but that page doesn't exist!</p>
+      {user && (
+        <h2>
+          Unable to find user <span style={{ color: 'red' }}>{user}</span>
+        </h2>
+      )}
+      {!user && <h2>Page Not Found</h2>}
+      <p>Sorry, but we looked far and wide and couldn't find it!</p>
       <br />
       <Link to="/">Go back</Link>
     </div>
