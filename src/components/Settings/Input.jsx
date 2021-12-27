5fa4536ffd25518dd89232259274ce47004ce720
Ofek Asido <asido.ofek@gmail.com>
2021-12-28 00:16:39 +0200

    Pass onChange as prop in input component

--- src/components/Settings/Input.jsx
+++ src/components/Settings/Input.jsx
@@ -1,4 +1,6 @@
-const Input = ({ className, fieldName, type, isTextArea }) => {
+import { useState } from 'react';
+
+const Input = ({ className, fieldName, name, type, isTextArea, onChange }) => {
   if (className === undefined) {
     className = '';
   }
@@ -16,13 +18,13 @@ const Input = ({ className, fieldName, type, isTextArea }) => {
       {!isTextArea && (
         <div className="field">
           <label>{fieldName}</label>
-          <input className={'settings-input' + className} type={type} />
+          <input onChange={onChange} name={name} className={'settings-input' + className} type={type} />
         </div>
       )}
       {isTextArea && (
         <div className="field">
           <label>{fieldName}</label>
-          <textarea rows="3" className={'settings-input' + className}></textarea>
+          <textarea onChange={onChange} name={name} rows="3" className={'settings-input' + className}></textarea>
         </div>
       )}
     </>
