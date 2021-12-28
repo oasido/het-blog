aeb86b90508b158db38b900d94f62715620cb639
Ofek Asido <asido.ofek@gmail.com>
2021-12-28 23:57:57 +0200

    Add required property

--- src/components/Settings/Input.jsx
+++ src/components/Settings/Input.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const Input = ({ className, fieldName, name, type, isTextArea, onChange }) => {
+const Input = ({ className, fieldName, name, type, isTextArea, onChange, required }) => {
   if (className === undefined) {
     className = '';
   }
@@ -18,13 +18,13 @@ const Input = ({ className, fieldName, name, type, isTextArea, onChange }) => {
       {!isTextArea && (
         <div className="field">
           <label>{fieldName}</label>
-          <input onChange={onChange} name={name} className={'settings-input' + className} type={type} />
+          <input required={required} onChange={onChange} name={name} className={'settings-input' + className} type={type} />
         </div>
       )}
       {isTextArea && (
         <div className="field">
           <label>{fieldName}</label>
-          <textarea onChange={onChange} name={name} rows="3" className={'settings-input' + className}></textarea>
+          <textarea required={required} onChange={onChange} name={name} rows="3" className={'settings-input' + className}></textarea>
         </div>
       )}
     </>
