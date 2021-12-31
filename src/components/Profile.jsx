ca102f2b4c0e99550da1e6e16b7dd0e805fb81e4
Ofek Asido <asido.ofek@gmail.com>
2021-12-31 18:19:41 +0200

    Add user's posts in profiles

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import ComponentLoader from './ComponentLoader';
 import ProfilePicture from './ProfilePicture';
 import NotFound from './NotFound';
@@ -17,7 +17,8 @@ const Profile = () => {
   const [userGithub, setUserGithub] = useState(null);
   const [userTwitter, setUserTwitter] = useState(null);
   const [userLocation, setUserLocation] = useState(null);
-  const [avatar, setAvatar] = useState(`/profile-pictures/${userID}`);
+  const [avatar, setAvatar] = useState('');
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const getUser = async () => {
@@ -37,62 +38,95 @@ const Profile = () => {
     };
     getUser();
 
+    const getUserPosts = async () => {
+      const response = await fetch('/api/user-posts/' + usernameParams, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      setPosts(data);
+    };
+    getUserPosts();
+
     // eslint-disable-next-line
   }, []);
 
-  const navigate = useNavigate();
-
-  if (isAuthenticated) {
-    console.log('logged in');
-  }
+  // posts function
+  const handlePosts = (post) => {
+    return (
+      <div className="blog-preview" key={post._id}>
+        <Link to={`/blogs/${post._id}`}>
+          <div className="top-bar">
+            <ProfilePicture className="author-picture" src={avatar} />
+            <div>
+              <p className="author-text">{post.author}</p>
+              <p className="time-posted">{post.date}</p>
+            </div>
+            {post.pinned && <div className="blog-pin">📌 Pinned</div>}
+          </div>
+          <h2 className="blog-title">{post.title}</h2>
+          <h4 className="blog-description">{post.description}</h4>
+        </Link>
+      </div>
+    );
+  };
 
   return (
-    <div className="profile">
-      {userID !== 'UserNotFound' && (
-        <>
-          <div className="profile-picture">
-            <ProfilePicture className="user-avatar big" src={avatar} />
-          </div>
-          <div className="profile-content">
-            <h1 className="profile-name">{usernameParams}</h1>
-            {userBio ? (
-              <p>{userBio}</p>
-            ) : (
-              <p>
-                <i>Hello world!</i>
-              </p>
-            )}
-          </div>
-          <div className="profile-bottom">
-            {userMemberSince && (
-              <p>
-                <GoOrganization />
-                Joined on {userMemberSince}
-              </p>
-            )}
-            {userLocation && (
-              <p>
-                <GoLocation />
-                {userLocation}
-              </p>
-            )}
-            {userGithub && (
-              <p>
-                <GoMarkGithub />
-                {userGithub}
-              </p>
-            )}
-            {userTwitter && (
-              <p>
-                <FaTwitter />
-                {userTwitter}
-              </p>
-            )}
-          </div>
-        </>
-      )}
-      {userID === 'UserNotFound' && <NotFound user={usernameParams} />}
-    </div>
+    <>
+      <div className="profile">
+        {userID !== 'UserNotFound' && (
+          <>
+            <div className="profile-picture">
+              <ProfilePicture className="user-avatar big" src={avatar} />
+            </div>
+            <div className="profile-content">
+              <h1 className="profile-name">{usernameParams}</h1>
+              {userBio ? (
+                <p>{userBio}</p>
+              ) : (
+                <p>
+                  <i>Hello world!</i>
+                </p>
+              )}
+            </div>
+            <div className="profile-bottom">
+              {userMemberSince && (
+                <p>
+                  <GoOrganization />
+                  Joined on {userMemberSince}
+                </p>
+              )}
+              {userLocation && (
+                <p>
+                  <GoLocation />
+                  {userLocation}
+                </p>
+              )}
+              {userGithub && (
+                <p>
+                  <GoMarkGithub />
+                  {userGithub}
+                </p>
+              )}
+              {userTwitter && (
+                <p>
+                  <FaTwitter />
+                  {userTwitter}
+                </p>
+              )}
+            </div>
+          </>
+        )}
+        {userID === 'UserNotFound' && <NotFound user={usernameParams} />}
+      </div>
+      <div className="profile-posts">
+        {!posts.length && <h2>This user hasn't posted anything, yet!</h2>}
+        {posts.length >= 1 && <h2>Posts by {usernameParams}</h2>}
+      </div>
+
+      {posts.map((post) => handlePosts(post))}
+    </>
   );
 };
 
