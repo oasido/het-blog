608b4505d600461ec464d6be8f19f81aabc5b65c
Ofek Asido <asido.ofek@gmail.com>
2021-12-31 02:45:47 +0200

    Add value attribute to Input component

--- src/components/Settings/Input.jsx
+++ src/components/Settings/Input.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const Input = ({ className, fieldName, name, type, isTextArea, onChange, required }) => {
+const Input = ({ className, fieldName, name, type, isTextArea, onChange, required, value }) => {
   if (className === undefined) {
     className = '';
   }
@@ -18,13 +18,13 @@ const Input = ({ className, fieldName, name, type, isTextArea, onChange, require
       {!isTextArea && (
         <div className="field">
           <label>{fieldName}</label>
-          <input required={required} onChange={onChange} name={name} className={'settings-input' + className} type={type} />
+          <input required={required} value={value} onChange={onChange} name={name} className={'settings-input' + className} type={type} />
         </div>
       )}
       {isTextArea && (
         <div className="field">
           <label>{fieldName}</label>
-          <textarea required={required} onChange={onChange} name={name} rows="3" className={'settings-input' + className}></textarea>
+          <textarea required={required} value={value} onChange={onChange} name={name} rows="3" className={'settings-input' + className}></textarea>
         </div>
       )}
     </>
