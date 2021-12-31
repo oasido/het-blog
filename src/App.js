85edf73a7b4cf385960f60170f88da88aa3f7d28
Ofek Asido <asido.ofek@gmail.com>
2021-12-31 18:21:35 +0200

    Update TODOs

--- src/App.js
+++ src/App.js
@@ -13,18 +13,23 @@
 // DONE: Fix avatars so that they will show the correct ones for users / ON HOLD => API for pictures
 // DONE: Learn React Context https://reactjs.org/docs/context.html#dynamic-context
 // DONE: Change from POST to GET
-// TODO: Add user settings page
+// DONE: Add user settings page
+// DONE: Posts by user (in profile)
+// TODO: Upload button beautify + show errors
 // TODO: Likes feature
+// TODO: Phone compatability!!!
 // TODO: Add admin page where you can pin posts & manage users (maybe add canPost in user model)
 // TODO: Add markdown support for adding blogs, pictures in blog
 // TODO: Add commenting system
 // TODO: Add edit functionality
-// MINOR: Convert to axios?
+// TODO: Pages functionality
+// MINOR: Go through errors/unused variables
 // MINOR: Value=value
 // MINOR: Navbar hide create post button when on create post page, show Create Post as text
 // MINOR: Rearrange every component into subfolders
 // MINOR: Add minimum user/password length
 // MINOR: Ternary operator if DB is off
+// MINOR: Convert to axios?
 // IDEA: User posts in their profiles, followers?
 
 import { useEffect, useState } from 'react';
